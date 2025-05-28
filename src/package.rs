1f4a0e852af807f3fbb6fe631bc0323650e33082
Erik Hollensbe <git@hollensbe.org>
2025-05-27 22:17:27 -0700

    more cleanup
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/package.rs
+++ src/package.rs
@@ -1,14 +1,25 @@
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Package {
     pub title: PackageTitle,
     pub description: String,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub dependencies: Vec<PackageTitle>,
     pub source: Source,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub networking: Option<Networking>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub storage: Option<Storage>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub system: Option<System>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub resources: Option<Resources>,
 }
 
@@ -64,38 +75,89 @@ pub enum Source {
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Networking {
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub forward_ports: Vec<u16>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub expose_ports: Vec<u16>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub internal_network: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub hostname: Option<String>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Storage {
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub volumes: Vec<Volume>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Volume {
-    name: String,
-    size: u64,
-    recreate: bool,
-    private: bool,
+    pub name: String,
+    pub size: u64,
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub recreate: bool,
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub private: bool,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct System {
     // --pid host
-    host_pid: bool,
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub host_pid: bool,
     // --net host
-    host_net: bool,
-    capabilities: Vec<String>,
-    privileged: bool,
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub host_net: bool,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub capabilities: Vec<String>,
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub privileged: bool,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Resources {
-    cpus: u8,
-    memory: u32,
+    pub cpus: u8,
+    pub memory: u32,
     // probably something to bring in PCI devices to appease the crypto folks
+}
+
+impl Package {
+    pub fn from_file(name: &Path) -> Result<Self> {
+        Ok(serde_json::from_reader(
+            std::fs::OpenOptions::new().open(name)?,
+        )?)
+    }
+
+    pub fn load(root: &Path, name: String, version: String) -> Result<Self> {
+        let pb = PathBuf::from_str(root.as_os_str().try_into()?)?.join("packages");
+        let name = pb.join(name).join(&format!("{}.json", version));
+        Ok(serde_json::from_reader(
+            std::fs::OpenOptions::new().open(name)?,
+        )?)
+    }
+
+    pub fn write(&self, root: &Path) -> Result<()> {
+        let pb = PathBuf::from_str(root.as_os_str().try_into()?)?.join("packages");
+        std::fs::create_dir_all(&pb)?;
+
+        let name = pb
+            .join(&self.title.name)
+            .join(&format!("{}.json", self.title.version));
+
+        Ok(serde_json::to_writer(
+            std::fs::OpenOptions::new()
+                .create_new(true)
+                .truncate(true)
+                .write(true)
+                .open(name)?,
+            self,
+        )?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_io() {}
 }
