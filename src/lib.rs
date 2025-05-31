21d2928fc87a0266e2e33d7dddb5423c48424510
Erik Hollensbe <git@hollensbe.org>
2025-05-31 11:34:08 -0700

    move input to own file
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod globals;
+mod input;
 mod package;
 mod prompt;
 
 pub use globals::*;
+pub use input::*;
 pub use package::*;
 pub use prompt::*;
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,3 +1,4 @@
+use crate::{Input, InputType};
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 
@@ -99,61 +100,6 @@ pub struct PromptCollection(Vec<Prompt>);
 impl PromptCollection {
     pub fn to_vec(&self) -> Vec<Prompt> {
         self.0.clone()
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-pub enum InputType {
-    #[serde(rename = "integer")]
-    Integer,
-    #[serde(rename = "signed_integer")]
-    SignedInteger,
-    #[serde(rename = "select")]
-    Select(Vec<SelectOption>),
-    #[serde(rename = "name")]
-    Name,
-    #[serde(rename = "path")]
-    Path,
-    #[serde(rename = "boolean")]
-    Boolean,
-    List,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-pub struct SelectOption {
-    pub name: String,
-    pub value: Input,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-pub enum Input {
-    #[serde(rename = "integer")]
-    Integer(u64),
-    #[serde(rename = "signed_integer")]
-    SignedInteger(i64),
-    #[serde(rename = "string")]
-    String(String),
-    #[serde(rename = "boolean")]
-    Boolean(bool),
-    List(Vec<Input>),
-}
-
-impl ToString for Input {
-    fn to_string(&self) -> String {
-        match self {
-            Input::Integer(x) => x.to_string(),
-            Input::SignedInteger(x) => x.to_string(),
-            Input::String(x) => x.to_string(),
-            Input::Boolean(x) => x.to_string(),
-            Input::List(x) => format!(
-                "[{}]",
-                x.iter()
-                    .map(ToString::to_string)
-                    .collect::<Vec<String>>()
-                    .join(", ")
-                    .to_string()
-            ),
-        }
     }
 }
 
