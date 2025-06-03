8d48d209a23e932bcea35b01e41eca0e08f33afa
Erik Hollensbe <git@hollensbe.org>
2025-06-03 15:56:35 -0700

    templating of globals
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/globals.rs
+++ src/globals.rs
@@ -1,8 +1,9 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, path::PathBuf};
 
 const GLOBAL_SUBPATH: &str = "variables";
+const DELIMITER: char = '@';
 
 pub type Variables = HashMap<String, String>;
 
@@ -15,6 +16,51 @@ pub struct Global {
 impl Global {
     pub fn var(&self, name: &str) -> Option<String> {
         self.variables.get(name).cloned()
+    }
+
+    pub fn template(&self, s: String) -> Result<String> {
+        let mut tmp = String::new();
+        let mut inside = false;
+        let mut out = String::new();
+
+        for ch in s.chars() {
+            if inside && ch == DELIMITER {
+                inside = false;
+                if tmp.is_empty() {
+                    // @@, not a template
+                    out.push(DELIMITER);
+                    continue;
+                }
+
+                let mut matched = false;
+                for (key, value) in &self.variables {
+                    if key == &tmp {
+                        out += value.as_str();
+                        matched = true;
+                        break;
+                    }
+                }
+
+                if !matched {
+                    return Err(anyhow!("No response matches prompt '{}'", tmp));
+                }
+
+                tmp = String::new();
+            } else if ch == DELIMITER {
+                inside = true
+            } else if inside {
+                tmp.push(ch)
+            } else {
+                out.push(ch)
+            }
+        }
+
+        // if we were inside at the end of the string, don't swallow the ?
+        if inside {
+            out += &(DELIMITER.to_string() + &tmp);
+        }
+
+        Ok(out)
     }
 }
 
@@ -113,5 +159,36 @@ mod tests {
             assert!(registry.set(item).is_ok());
             assert_eq!(registry.get(&item.name).unwrap(), item.clone());
         }
+    }
+
+    #[test]
+    fn template() {
+        let mut variables = Variables::default();
+        variables.insert("foo".into(), "bar".into());
+        variables.insert("baz".into(), "quux".into());
+
+        let global = Global {
+            name: "test".into(),
+            variables: variables.clone(),
+        };
+
+        assert!(global.template("@nonexistent@".into()).is_err());
+        assert_eq!(global.template("@foo@".into(),).unwrap(), "bar");
+        assert_eq!(global.template("@foo@ @baz@".into(),).unwrap(), "bar quux");
+
+        assert!(global.template("@foo".into()).is_ok());
+        assert_eq!(global.template("@foo".into()).unwrap(), "@foo");
+        assert!(global.template("@".into()).is_ok());
+        assert_eq!(global.template("@".into()).unwrap(), "@");
+        assert!(global.template("@@".into()).is_ok());
+        assert_eq!(global.template("@@".into()).unwrap(), "@");
+        assert_eq!(
+            global.template("bgates@microsoft.com".into()).unwrap(),
+            "bgates@microsoft.com"
+        );
+        assert_eq!(
+            global.template("bgates@@microsoft.com".into()).unwrap(),
+            "bgates@microsoft.com"
+        );
     }
 }
