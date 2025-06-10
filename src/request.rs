c9c0979b8a53ceee4d3f80696c83129c1f1a2373
Erik Hollensbe <git@hollensbe.org>
2025-06-10 10:22:17 -0700

    preemptive strike
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/request.rs
+++ src/request.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::{net::SocketAddr, path::PathBuf};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Request {
     Port(u16),
     Volume(PathBuf),
+    Network(SocketAddr),
 }
