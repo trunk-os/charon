3af696462cad4c265ea4cde034a3b17137f1085b
Erik Hollensbe <git@hollensbe.org>
2025-06-11 16:03:42 -0700

    oops
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 mod cli;
 mod globals;
 mod input;
-mod launcher;
 mod package;
 mod prompt;
 
 pub use cli::*;
 pub use globals::*;
 pub use input::*;
-pub use launcher::*;
 pub use package::*;
 pub use prompt::*;
