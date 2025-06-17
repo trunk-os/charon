19c95925ad125ef8ecf063aca0930385c3508724
Erik Hollensbe <git@hollensbe.org>
2025-06-17 13:35:55 -0700

    first stab at some systemd tooling
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,11 @@ mod package;
 mod prompt;
 #[allow(dead_code)]
 pub(crate) mod qmp;
+mod systemd;
 
 pub use cli::*;
 pub use globals::*;
 pub use input::*;
 pub use package::*;
 pub use prompt::*;
+pub use systemd::*;
---
+++ src/systemd.rs
@@ -0,0 +1,63 @@
+use crate::CompiledPackage;
+use anyhow::{anyhow, Result};
+use std::path::PathBuf;
+
+const SYSTEMD_SERVICE_ROOT: &str = "/etc/systemd/system";
+
+const UNIT_TEMPLATE: &str = r#"
+[Unit]
+Description=Charon launcher for @PACKAGE_NAME@, version @PACKAGE_VERSION@
+After= # FIXME: this needs to follow the trunk microservices boot
+
+[Service]
+ExecStart=/usr/bin/charon -r @REGISTRY@ launch @PACKAGE_NAME@ @PACKAGE_VERSION@ @VOLUME_ROOT@
+ExecStop=/usr/bin/charon -r @REGISTRY@ stop @PACKAGE_NAME@ @PACKAGE_VERSION@ @VOLUME_ROOT@
+Restart=always
+
+[Install]
+Alias=@PACKAGE_FILENAME@.service
+"#;
+
+#[derive(Debug, Clone)]
+pub struct SystemdUnit {
+    package: CompiledPackage,
+}
+
+impl SystemdUnit {
+    pub fn new(package: CompiledPackage) -> Self {
+        Self { package }
+    }
+
+    pub fn filename(&self) -> PathBuf {
+        format!("/{}/{}.service", SYSTEMD_SERVICE_ROOT, self.package.title).into()
+    }
+
+    pub fn unit(&self, volume_root: PathBuf) -> Result<String> {
+        let mut out = String::new();
+        let mut variable = String::new();
+        let mut in_variable = false;
+
+        for ch in UNIT_TEMPLATE.chars() {
+            if ch == '@' {
+                in_variable = if in_variable {
+                    match variable.as_str() {
+                        "PACKAGE_NAME" => out.push_str(&self.package.title.name),
+                        "PACKAGE_VERSION" => out.push_str(&self.package.title.version),
+                        "PACKAGE_FILENAME" => out.push_str(&self.package.title.to_string()),
+                        "VOLUME_ROOT" => out.push_str(volume_root.to_str().unwrap_or_default()),
+                        _ => return Err(anyhow!("invalid template variable '{}'", variable)),
+                    }
+                    false
+                } else {
+                    true
+                }
+            } else if in_variable {
+                variable.push(ch)
+            } else {
+                out.push(ch)
+            }
+        }
+
+        Ok(out)
+    }
+}
