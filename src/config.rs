410025bcaea35ef499d88690cd7f71aa9095979e
Erik Hollensbe <git@hollensbe.org>
2025-06-21 10:55:17 -0700

    logging
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,66 @@
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::path::PathBuf;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Default)]
+pub enum LogLevel {
+    #[serde(rename = "warn")]
+    Warn,
+    #[default]
+    #[serde(rename = "info")]
+    Info,
+    #[serde(rename = "error")]
+    Error,
+    #[serde(rename = "debug")]
+    Debug,
+    #[serde(rename = "trace")]
+    Trace,
+}
+
+impl From<LogLevel> for tracing::Level {
+    fn from(value: LogLevel) -> Self {
+        match value {
+            LogLevel::Info => tracing::Level::INFO,
+            LogLevel::Warn => tracing::Level::WARN,
+            LogLevel::Error => tracing::Level::ERROR,
+            LogLevel::Debug => tracing::Level::DEBUG,
+            LogLevel::Trace => tracing::Level::TRACE,
+        }
+    }
+}
+
+impl From<tracing::Level> for LogLevel {
+    fn from(value: tracing::Level) -> Self {
+        match value {
+            tracing::Level::INFO => LogLevel::Info,
+            tracing::Level::WARN => LogLevel::Warn,
+            tracing::Level::ERROR => LogLevel::Error,
+            tracing::Level::DEBUG => LogLevel::Debug,
+            tracing::Level::TRACE => LogLevel::Trace,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct Config {
     pub registry: PathBuf,
     pub socket: PathBuf,
+    pub log_level: Option<LogLevel>,
 }
 
 impl Config {
     pub fn from_file(filename: PathBuf) -> Result<Self> {
         let f = std::fs::OpenOptions::new().read(true).open(&filename)?;
-        Ok(serde_yaml_ng::from_reader(&f)?)
+        let this: Self = serde_yaml_ng::from_reader(&f)?;
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(Into::<tracing::Level>::into(
+                this.log_level.clone().unwrap_or_default(),
+            ))
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)?;
+        info!("Configuration parsed successfully.");
+        Ok(this)
     }
 }
